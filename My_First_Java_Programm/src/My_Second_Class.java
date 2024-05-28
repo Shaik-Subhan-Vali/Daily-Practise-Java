d1777d07ad3e76935f5554bd0f265892489e1a5a
Shaik Subhan Vali <subhanshaik7781@gmail.com>
2024-05-28 08:56:05 +0530

    Update My_Second_Class.java

--- My_First_Java_Programm/src/My_Second_Class.java
+++ My_First_Java_Programm/src/My_Second_Class.java
@@ -4,6 +4,7 @@ public class My_Second_Class {
 	public static void main(String[] args) 
 	{
 		System.out.println("My Second programme in java");
+		System.out.println("I made some changes in git repo");
 
 	}
 
