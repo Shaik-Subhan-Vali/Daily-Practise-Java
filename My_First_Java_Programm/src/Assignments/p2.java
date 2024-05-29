df7d44c87cbfcd4b03ec3ddf9645d19fe4d559c8
subhanlucky <83771419+subhanlucky@users.noreply.github.com>
2024-05-29 20:00:31 +0530

    f commit

---
+++ My_First_Java_Programm/src/Assignments/p2.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class p2 {
+
+	public static void main(String[] args) {
+		int a =10;
+		int x = a++;
+		int y =++a;
+		
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+	
+
+}
